70437857ad6441dfda494cb8b183638ae2e03884
nurdeniz <nurdenizkaradeniz6@gmail.com>
2026-05-09 14:32:49 +0300

    Shield SO

---
+++ Assets/Scripts/WeaponScripts/Shield.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Shield", menuName = "ScriptableObjects/WeaponBase/Shield")]
+
+public class Shield : WeaponBase
+{
+
+    public override void Activate1(GameObject owner)
+    {
+        //swing
+    }
+
+    public override void Activate2(GameObject owner)
+    {
+
+    }
+}
