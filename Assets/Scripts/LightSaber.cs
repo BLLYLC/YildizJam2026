b90f77985bc3ea2fed45f36c9a37dd69b6f574c2
Deniz Yılmaz <den1zkz@yandex.com>
2026-05-09 15:27:11 +0300

    LightSaber coded.

---
+++ Assets/Scripts/LightSaber.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class LightSaber : WeaponBase
+{
+    [SerializeField] Transform hitPos;
+    [SerializeField] float damage;
+    public override void Activate1(GameObject owner)
+    {
+        //Normal attack
+        //2 = animasyon + saldýrý
+
+        //hit
+        var hit = Physics.OverlapSphere(hitPos.position, 1);
+        foreach(Collider h in hit)
+        {
+            if(h.TryGetComponent<PlayerStats>(out PlayerStats p))
+            {
+                p.TakeDamage(damage);
+            }
+        }
+    }
+
+    public override void Activate2(GameObject owner)
+    {
+        //None
+    }
+}
