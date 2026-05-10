8cb72b03be162cf07fbdbe038db6f00cf8e6d2cf
nurdeniz <nurdenizkaradeniz6@gmail.com>
2026-05-10 12:16:31 +0300

    table prefabs and menu scene

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class gameobj : MonoBehaviour
+public class MainMenuManager : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
