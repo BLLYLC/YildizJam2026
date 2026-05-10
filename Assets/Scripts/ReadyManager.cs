c93e90f7ea12c653454f29af5de776c80552be43
BLLYLC <yolcubilal97@gmail.com>
2026-05-10 19:56:05 +0300

    managers

--- Assets/Scripts/ReadyManager.cs
+++ Assets/Scripts/ReadyManager.cs
@@ -1,13 +1,18 @@
 using System;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ReadyManager : MonoBehaviour
 {
     [SerializeField] private ReadyStates p1State;
     [SerializeField] private ReadyStates p2State;
+    [SerializeField] private GameObject p1ReadyBackground;
+    [SerializeField] private GameObject p2ReadyBackground;
+    [SerializeField] private TextMeshProUGUI countdownTMP;
     //private event EventHandler timerStarted;
-    private float timer=0f;
-    private float readyCooldown = 3f;
+    private float timer=4f;
+    private float readyCooldown = 4f;
     private enum ReadyStates
     {
         NOT_READY,
@@ -32,9 +37,11 @@ public class ReadyManager : MonoBehaviour
             if (p1State == ReadyStates.READY)
             {
                 p1State = ReadyStates.NOT_READY;
+                p1ReadyBackground.SetActive(false);
             }
             else if (p1State == ReadyStates.NOT_READY) { 
-                p1State  = ReadyStates.READY; 
+                p1State  = ReadyStates.READY;
+                p1ReadyBackground.SetActive(true);
             }
         }
         else if (pID == 1)
@@ -42,10 +49,12 @@ public class ReadyManager : MonoBehaviour
             if (p2State == ReadyStates.READY)
             {
                 p2State = ReadyStates.NOT_READY;
+                p2ReadyBackground.SetActive(false);
             }
             else if (p2State == ReadyStates.NOT_READY)
             {
                 p2State = ReadyStates.READY;
+                p2ReadyBackground.SetActive(true);
             }
         }
     }
@@ -53,16 +62,18 @@ public class ReadyManager : MonoBehaviour
     {
         if(p1State == ReadyStates.READY && p2State == ReadyStates.READY)
         {
+            countdownTMP.text = ((int)timer).ToString();
             timer -= Time.deltaTime;
             if (timer < 0)
             {
                 GameManager.Instance.StartGame();
-                timer = 3;
+                timer = 4;
             }
            
         }
         else
         {
+            countdownTMP.text = "";
             timer = readyCooldown;
         }
     }
